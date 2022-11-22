2fade3ab4182835530bfc19e1ca9e07304585d4d
IrJusticeKid <118821493+IrJusticeKid@users.noreply.github.com>
2022-11-22 15:33:17 +0000

    Create serviceworker.js

---
+++ serviceworker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "version-00";
+const urlsToCache = ['kaveh.html'];
+
+// Install the service worker and open the cache and add files mentioned in array to cache
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Listens to request from application.
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    if (response) {
+                        // The requested file exists in cache so we return it from cache.
+                        return response;
+                    }
+
+                    return fetch(event.request)
+                        .catch(err => {
+                            return caches.match('kaveh.html');
+                        });
+                }
+            )
+    );
+});
+
+
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = []; // add cache names which you do not want to delete
+    cacheWhitelist.push(CACHE_NAME);
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
